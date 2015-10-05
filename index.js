ddcf99c2ed165d6284f81b4523cfcb0ebad10b15
Jordan Drake <jordan@jdrake.co.uk>
2015-10-05 09:45:03 +0100

    Added pagination for S3

--- index.js
+++ index.js
@@ -5,36 +5,58 @@ var s3 = new AWS.S3();
 var urlPrefix = 'http://dm8eklel4s62k.cloudfront.net/images/';
 var foldersRegex = /images\/(small|medium|large)\/(.*)/;
 
-function search() {
-  var rl = readline.createInterface({
-    input: process.stdin,
-    output: process.stdout
-  });
+function search(term, nextMarker, acc) {
+    var settings = {
+        Bucket: 'polaris-assets',
+        Prefix: 'images'
+    };
 
-  rl.question("Search: ", function(answer) {
-    rl.close();
+    if (nextMarker) {
+        settings['Marker'] = nextMarker;
+    }
 
-    s3.listObjects({
-      Bucket: 'polaris-assets',
-      Prefix: 'images'
-    }, function (err, data) {
-      var collection = {};
-      collection['panelImage'] = collection['tileImage'] = {};
-      data.Contents.forEach(function (item) {
-        if(item.Key.toLowerCase().indexOf(answer.toLowerCase()) > -1) {
-          var parts = item.Key.match(foldersRegex);
-          if(parts) {
-            var size = parts[1];
-            var filename = encodeURIComponent(parts[2]);
-
-            collection['panelImage'][size] = urlPrefix + size + "/" + filename;
-          }
+    s3.listObjects(settings, function(err, data) {
+        var marker;
+        var collection = acc;
+        if (acc) {
+            collection = acc;
+        } else {
+            collection = {};
+            collection['panelImage'] = collection['tileImage'] = {};
         }
-      });
-      console.log('"tileImage":', JSON.stringify(collection['tileImage'],null, ' ') + ',');
-      console.log('"panelImage":', JSON.stringify(collection['panelImage'],null, ' ') + ',');
-      search();
+        data.Contents.forEach(function(item) {
+            marker = item.Key;
+            if (item.Key.toLowerCase().indexOf(term.toLowerCase()) > -1) {
+                var parts = item.Key.match(foldersRegex);
+                if (parts) {
+                    var size = parts[1];
+                    var filename = encodeURIComponent(parts[2]);
+
+                    collection['panelImage'][size] = urlPrefix + size + "/" + filename;
+                }
+            }
+        });
+        if (data.IsTruncated) {
+            search(term, marker, collection);
+        } else {
+            console.log('"tileImage":', JSON.stringify(collection['tileImage'], null, ' ') + ',');
+            console.log('"panelImage":', JSON.stringify(collection['panelImage'], null, ' ') + ',');
+            promptForSearch();
+        }
     });
-  });
 }
-search();
+
+function promptForSearch() {
+    var rl = readline.createInterface({
+        input: process.stdin,
+        output: process.stdout
+    });
+
+    rl.question("Search: ", function(answer) {
+        rl.close();
+
+        search(answer);
+    });
+}
+
+promptForSearch();
